e521095ee9836210762d27b68fcc5c352f7385dd
Palina Apanovich <palina.apanovich@gmail.com>
2024-03-26 19:46:36 +0100

    compiler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CampaignsModule } from './campaigns/campaigns.module';
-import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, TypeaheadModule.forRoot(), CampaignsModule],
+  imports: [BrowserModule, FormsModule, CampaignsModule],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
